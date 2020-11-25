3e224f4c6e927ebf1ac5342b0fa465f04b5bbf92
Mayank52 <43692474+Mayank52@users.noreply.github.com>
2020-11-25 20:05:12 +0530

    Update receiver.js

--- receiver.js
+++ receiver.js
@@ -15,3 +15,22 @@ socket.on("onmousemove", function (point) {
   ctx.lineTo(x, y);
   ctx.stroke();
 });
+
+socket.on("redraw", (points) => {
+  console.log("redraw reached");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < points.length; i++) {
+    let point = points[i];
+    ctx.lineWidth = point.width;
+    ctx.strokeStyle = point.color;
+    if (point.id == "md") {
+      ctx.beginPath();
+      ctx.moveTo(point.x, point.y);
+    } else {
+      ctx.lineTo(point.x, point.y);
+      ctx.stroke();
+    }
+  }
+  ctx.strokeStyle = currStrokeStyle;
+  ctx.lineWidth = currLineWidth;
+});
